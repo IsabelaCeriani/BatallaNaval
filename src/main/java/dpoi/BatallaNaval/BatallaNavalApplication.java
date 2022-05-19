13cde2a10f7ef019ecee6f056e951583dd520cd8
Isabela Ceriani <isabela.ceriani@ing.austral.edu.ar>
2022-05-18 22:45:12 -0300

    Initial commit

---
+++ src/main/java/dpoi/BatallaNaval/BatallaNavalApplication.java
@@ -0,0 +1,13 @@
+package dpoi.BatallaNaval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatallaNavalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatallaNavalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dpoi/BatallaNaval/BatallaNavalApplicationTests.java
@@ -0,0 +1,13 @@
+package dpoi.BatallaNaval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatallaNavalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
